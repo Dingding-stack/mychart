03e7ae069396c8dd3f4a7313f9691fcad5c7541d
dingding <dfdf@qq.com>
2024-12-18 10:38:31 +0800

    finishing register and login

--- js/common.js
+++ js/common.js
@@ -5,3 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('ready')
   }, 200)
 })
+
+function tip(msg = 'hints'){
+  const toastBox =  document.querySelector('#myToast')
+			//the boostrap
+			const toast = new bootstrap.Toast(toastBox, {
+				animation: true,
+				autohide: true,
+				delay: 2000,
+
+
+			})
+			toastBox.querySelector('.toast-body').innerHTML = msg
+
+			toast.show()
+} 
+
+//setting request root url
+axios.defaults.baseURL = 'http://ajax-api.itheima.net'
